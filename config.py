ab2e985078f191caa86b03b4e03f1442285c39d2
Juan <jumasaa7x@gmail.com>
2025-12-20 07:41:16 -0300

    Corrected unused code in remaining files

--- config.py
+++ config.py
@@ -4,16 +4,3 @@ import os
 # Configuración general
 SERIAL_PORT = "COM5"     # Ajustar al puerto real   
 BAUDRATE = 9600
-DISPLAY_LAST = 100
-USE_SIMULATOR = False        # True = simulador, False = puerto real
-
-# Carpeta data relativa al proyecto
-BASE_DIR = os.path.dirname(__file__)  
-DATA_DIR = os.path.join(BASE_DIR, "data")
-
-# Crear la carpeta si no existe
-os.makedirs(DATA_DIR, exist_ok=True)
-
-# --- Archivos CSV ---
-RAW_CSV = os.path.join(DATA_DIR, "resultados_raw.csv")
-META_CSV = os.path.join(DATA_DIR, "resultados_meta.csv")
