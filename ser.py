d00c0db36c96dc8882538054347e02ba60c3c630
Juan <jumasaa7x@gmail.com>
2026-01-10 09:58:51 -0300

    Refactor rename

--- ser.py
+++ ser.py
@@ -1,20 +1,19 @@
 import serial
 import time
 from datetime import datetime
-from config import SERIAL_PORT, BAUDRATE
 
 def serial_tester():
     try:
         ser = serial.Serial(
-            port=SERIAL_PORT, # --- Si no reconoce el puerto, verificar en "adm de dispositivos" y corregir en config.py
-            baudrate=BAUDRATE, #--- No se cambia, está especificado en el manual
+            port="COM5",
+            baudrate=9600, #--- No se cambia, está especificado en el manual
             bytesize=serial.EIGHTBITS, #--- No se cambia, está especificado en el manual
             parity=serial.PARITY_NONE, #--- No se cambia, está especificado en el manual
             stopbits=serial.STOPBITS_ONE, #--- No se cambia, está especificado en el manual
             timeout=0.1
         )
 
-        print(f"Escuchando en {SERIAL_PORT} @ {BAUDRATE} baud...")
+        print(f"Escuchando...")
 
 
         today = datetime.now().strftime("%Y.%m.%d") #--- Extrae fecha de hoy, para usar en el nombre del archivo
@@ -36,8 +35,7 @@ def serial_tester():
                         ch = byte.decode("utf-8", errors="replace")
                     except Exception:
                         ch = str(byte)
-                    print(repr(ch), end="")  # repr() para ver \r o \n explícito
-
+                    print(ch) #print(repr(ch), end="")  para debug extra
                 except Exception as e:
                     print("Error leyendo serial:", e)
                     time.sleep(0.1)
