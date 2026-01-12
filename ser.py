5f52f46c5939486358714c495fc3ba6dce81b115
Juan <jumasaa7x@gmail.com>
2026-01-12 16:45:18 -0300

    Add smth

--- ser.py
+++ ser.py
@@ -6,36 +6,49 @@ def serial_tester():
     try:
         ser = serial.Serial(
             port="COM5",
-            baudrate=9600, #--- No se cambia, está especificado en el manual
-            bytesize=serial.EIGHTBITS, #--- No se cambia, está especificado en el manual
-            parity=serial.PARITY_NONE, #--- No se cambia, está especificado en el manual
-            stopbits=serial.STOPBITS_ONE, #--- No se cambia, está especificado en el manual
+            baudrate=9600, 
+            bytesize=serial.EIGHTBITS,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
             timeout=0.1
         )
 
-        print(f"Escuchando...")
+        print("Escuchando...")
 
+        today = datetime.now().strftime("%Y.%m.%d")
+        filename = f"{today} - datos_extraidos.txt"
 
-        today = datetime.now().strftime("%Y.%m.%d") #--- Extrae fecha de hoy, para usar en el nombre del archivo
-        filename = f"{today} - datos_extraidos.txt" # --- El nombre del archivo será igual a ej. "2025.11.24 - datos_extraidos.txt"
+        recibiendo = False
+        ultimo_byte = time.time()
+        TIEMPO_FIN = 5.0 
 
         with open(filename, "ab") as f:
             while True:
                 try:
                     byte = ser.read(1)
-                    if not byte:
-                        continue
 
-                    # --- Guardar el byte crudo en archivo ---
-                    f.write(byte)
-                    f.flush()  # asegura que se escriba al instante
+                    if byte:
+                        if not recibiendo:
+                            recibiendo = True
 
-                    # --- Mostrar en pantalla (solo para debug) ---
-                    try:
-                        ch = byte.decode("utf-8", errors="replace")
-                    except Exception:
-                        ch = str(byte)
-                    print(ch) #print(repr(ch), end="")  para debug extra
+                        ultimo_byte = time.time()
+
+                        # Guardar byte crudo
+                        f.write(byte)
+                        f.flush()
+
+                        # Mostrar en pantalla
+                        try:
+                            ch = byte.decode("utf-8", errors="replace")
+                        except Exception:
+                            ch = str(byte)
+                        print(ch) #print(repr(ch), end="")  para debug extra
+
+                    else:
+                        if recibiendo and (time.time() - ultimo_byte) >= TIEMPO_FIN:
+                            print("\nEscuchando...")
+                            recibiendo = False
+
                 except Exception as e:
                     print("Error leyendo serial:", e)
                     time.sleep(0.1)
